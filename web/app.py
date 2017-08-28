d997a247297862eb6a04050f81b0f3b03e028185
aloneZERO <alonezero@foxmail.com>
2017-08-28 12:59:07 +0800

    重构：评分页面
    评分页面功能重构

--- web/app.py
+++ web/app.py
@@ -37,32 +37,29 @@ def index():
 def rate():
     (db,cursor) = connectdb()
     try:
-        cursor.execute("select rate, district, category, showtime, length from movie")
+        cursor.execute("select rate, district, category, showtime, length from movie " + \
+                        "where showtime<>'未知' and length<>'-1'")
         movies = cursor.fetchall()
-        temp = []
-        for item in movies:
-            if not item['showtime'] == 0 and not item['length'] == 0:
-                temp.append(item)
-        movies = temp
         movies = json.dumps({"movies": movies})
-        cursor.execute("select * from rate")
+        cursor.execute("select name," + \
+                        "group_concat(concat_ws(',', category)) categories," + \
+                        "group_concat(concat_ws(',', rate)) rates " + \
+                        "from rate " + \
+                        "where category<>'未知' " + \
+                        "group by name")
         rates = cursor.fetchall()
         temp = {}
         for item in rates:
             temp[item['name']] = {}
             temp[item['name']]['categories'] = item['categories'].split(',')
             temp[item['name']]['rates'] = item['rates'].split(',')
-            array = []
-            for i in temp[item['name']]['rates']:
-                array.append(float(i))
-            temp[item['name']]['rates'] = array
+            temp[item['name']]['rates'] = list( map(float, temp[item['name']]['rates']) )
         rates = temp
-        pprint.pprint(rates)
         rates = json.dumps({"rates": rates})
     finally:
         closedb(db,cursor)
     categories = ['剧情','喜剧','惊悚','爱情','动作','悬疑','犯罪','恐怖','科幻','冒险','奇幻','家庭','动画','战争','历史','古装','传记','音乐','同性','武侠','情色','灾难','运动','歌舞','西部','儿童','黑色电影','鬼怪','荒诞']
-    districts = ['United States of America','China','Japan','South Korea','United Kingdom','France','Germany','Canada','Italy','Australia','Spain','Thailand','Russia','Belgium','Sweden','Ireland','Czech Republic','Denmark','India','Poland','Switzerland','New Zealand','Austria','Norway','Netherlands','Brazil','Hungary','Slovakia','Mexico','Iran','South Africa','Finland','Turkey','Romania','Luxembourg','Argentina','Iceland','Indonesia','Israel','United Arab Emirates','Malaysia','Georgia','Cuba','Kazakhstan','Estonia','Vietnam','Greece','Lithuania','Chile','Ukraine','Portugal','Bulgaria','Botswana','The Bahamas','Uzbekistan','Algeria','Puerto Rico','Philippines','Mauritania','Morocco','Latvia','Egypt','Myanmar','Tunisia','Peru','Colombia','Tajikistan']
+    districts = ['United States of America','China','Japan','South Korea','United Kingdom','France','Germany','Spain','Italy','Thailand','Canada','Russia','India','Australia','Ireland','Poland','Finland','Denmark','New Zealand','Czech Republic','Brazil','Sweden','Iran','Argentina','Belgium','Slovakia','Mexico','Norway','Austria','Netherlands','Chile','Hungary','Greece','Indonesia','Turkey','Switzerland','Iceland','Botswana','Malaysia','Israel','Romania','United Arab Emirates','Bulgaria','Myanmar','Bhutan','Armenia','Philippines','Panama','Portugal','Colombia','Luxembourg','Estonia','Uruguay','Slovenia','Georgia','Cuba','Vietnam']
     return render_template('rate.html',movies=movies,categories=categories,districts=districts,rates=rates)
 
 # 搜索页面
